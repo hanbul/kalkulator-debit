8375578d5903006c80ff5a4d6c70509dfebafbc4
handobel <hanhan.ahmad@gmail.com>
2026-06-22 17:27:49 +0900

    Initial development of the kalkulator debit web application (currently only for pintu sorong). Added the main HTML file, manifest for PWA support, and service worker for offline capabilities.

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE = 'kalkulator-debit-v1';
+const ASSETS = [
+  './index.html',
+  './debit-pintu-sorong.html',
+  './manifest.json',
+  './service-worker.js',
+  './assets/icon-192.png',
+  './assets/icon-512.png',
+  './assets/eq-info.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      if (resp) return resp;
+      return fetch(event.request).catch(() =>
+        caches.match('./debit-pintu-sorong.html')
+      );
+    })
+  );
+});
